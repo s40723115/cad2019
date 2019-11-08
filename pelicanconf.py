bd7d4d24fc24d6c7045b0c8f816193399544303b
mdecourse <mdecourse@gmail.com>
2019-11-08 19:28:20 +0800

    789

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'KMOL'
-SITENAME = 'CMSimfly 網際內容管理'
+AUTHOR = '40723115'
+SITENAME = 'Cad2019 電腦輔助設計'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
 
